c7237133353a5f458b79212eea6b8efeb01397b7
caio silva <caio_ceac23@hotmail.com>
2023-05-30 09:47:01 -0300

    up

--- crud_cbv/settings.py
+++ crud_cbv/settings.py
@@ -13,9 +13,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!&!2jb74awqbvd58c7)q$*m(*oz4p^h-a9^kdcmssi%q-c!x(o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ["DEBUG"]
 
-ALLOWED_HOSTS = ["*"]
+
+ALLOWED_HOSTS = ["127.0.0.1:8000"]
 
 
 # Application definition
